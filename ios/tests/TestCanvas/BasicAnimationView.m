6aa02fc8922f6d68ca73f0550c277bab5a4ef0b6
Zhang Yungui <rhcad@hotmail.com>
2013-04-13 11:05:09 +0800

    Add position's animation.

--- ios/tests/TestCanvas/BasicAnimationView.m
+++ ios/tests/TestCanvas/BasicAnimationView.m
@@ -12,14 +12,6 @@
     [super dealloc];
 }
 
-- (id)initWithFrame:(CGRect)frame withTests:(int)t
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-    }
-    return self;
-}
-
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesBegan:touches withEvent:event];
@@ -27,8 +19,8 @@
     UITouch *touch = [touches anyObject];
     CGPoint pt = [touch locationInView:touch.view];
     
-    float radius = 200.f;
-    float newRadius = 400.f;
+    float radius = 50.f;
+    float newRadius = 100.f;
     
     if (!shapeLayer) {
         shapeLayer = [[CAShapeLayer alloc]init];
@@ -36,46 +28,54 @@
         shapeLayer.fillColor = nil;	
         shapeLayer.strokeColor = [UIColor blackColor].CGColor;	
         shapeLayer.lineWidth = 7;	
-        shapeLayer.bounds = CGRectMake(pt.x - 2 * radius, pt.y - 2 * radius, 
-                                       2 * radius, 2 * radius);	
+        shapeLayer.bounds = CGRectMake(0, 0, 2 * radius, 2 * radius);	
         shapeLayer.path = [UIBezierPath bezierPathWithRect:shapeLayer.bounds].CGPath;
+        shapeLayer.position = pt;
         
         [self.layer addSublayer:shapeLayer];
     }
     else {
-        CGRect newBounds = CGRectMake(pt.x - 2 * newRadius, pt.y - 2 * newRadius, 
-                                      2 * newRadius, 2 * newRadius);
-        UIBezierPath *newPath;
+        CGRect newBounds = CGRectMake(0, 0, 2 * newRadius, 2 * newRadius);
+        CGPathRef newPath = NULL;
         CGColorRef fillColor = nil;
         
         if ((++step) % 3 == 1) {
-            newPath = [UIBezierPath bezierPathWithOvalInRect:newBounds];
+            newPath = [UIBezierPath bezierPathWithOvalInRect:newBounds].CGPath;
             fillColor = [UIColor clearColor].CGColor;
         }
         else if (step % 3 == 2) {
             newBounds = CGRectMake(pt.x - 2 * radius, pt.y - 2 * radius, 
                                    2 * radius, 2 * radius);
-            newPath = [UIBezierPath bezierPathWithOvalInRect:newBounds];
+            newPath = [UIBezierPath bezierPathWithOvalInRect:newBounds].CGPath;
             fillColor = [UIColor blueColor].CGColor;
         }
         else {
-            newPath = [UIBezierPath bezierPathWithRect:newBounds];
+            CGMutablePathRef path = CGPathCreateMutable();
+            CGPathMoveToPoint(path, NULL, 0, 100);
+            CGPathAddLineToPoint(path, NULL, 200, 0);
+            CGPathAddLineToPoint(path, NULL, 200,200);
+            CGPathAddLineToPoint(path, NULL, 0, 100);
+            CGPathCloseSubpath(path);
+            newPath = path;
             fillColor = [UIColor greenColor].CGColor;
         }
         
-        CABasicAnimation* pathAnim = [CABasicAnimation animationWithKeyPath: @"path"];	
-        pathAnim.toValue = (id)newPath.CGPath;
+        CABasicAnimation* pathAnim = [CABasicAnimation animationWithKeyPath: @"path"];
+        pathAnim.toValue = (id)newPath;
         
-        CABasicAnimation* boundsAnim = [CABasicAnimation animationWithKeyPath: @"bounds"];	
+        CABasicAnimation* boundsAnim = [CABasicAnimation animationWithKeyPath: @"bounds"];
         boundsAnim.toValue = [NSValue valueWithCGRect:newBounds];
         
-        CABasicAnimation* fillAnim = [CABasicAnimation animationWithKeyPath: @"fillColor"];	
+        CABasicAnimation* fillAnim = [CABasicAnimation animationWithKeyPath: @"fillColor"];
         fillAnim.toValue = (id)fillColor;
         
-        CAAnimationGroup *anims = [CAAnimationGroup animation];	
-        anims.animations = [NSArray arrayWithObjects:pathAnim, boundsAnim, fillAnim, nil];	
-        anims.removedOnCompletion = NO;	
-        anims.duration = 1.0f;
+        CABasicAnimation* positionAnim = [CABasicAnimation animationWithKeyPath: @"position"];
+        positionAnim.toValue = [NSValue valueWithCGPoint:pt];
+        
+        CAAnimationGroup *anims = [CAAnimationGroup animation];
+        anims.animations = [NSArray arrayWithObjects:pathAnim, boundsAnim, fillAnim, positionAnim, nil];
+        anims.removedOnCompletion = NO;
+        anims.duration = 0.3f;
         anims.fillMode  = kCAFillModeForwards;
         
         [shapeLayer addAnimation:anims forKey:nil];
