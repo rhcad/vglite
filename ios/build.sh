7467a9a23462d76bb2bed1f5203f08b97b7d7fc1
Zhang Yungui <rhcad@hotmail.com>
2013-06-27 10:03:13 +0800

    Fix the build script.

--- ios/build.sh
+++ ios/build.sh
@@ -8,12 +8,17 @@ mkdir build/iphonesimulator
 
 iphoneos61=`xcodebuild -showsdks | grep -i iphoneos6.1`
 iphoneos51=`xcodebuild -showsdks | grep -i iphoneos5.1`
+iphoneos43=`xcodebuild -showsdks | grep -i iphoneos4.3`
 
 if [ -n "$iphoneos61" ]; then
     xcodebuild -project framework/TouchVG.xcodeproj -sdk iphoneos6.1 -configuration Release
 else
 if [ -n "$iphoneos51" ]; then
     xcodebuild -project framework/TouchVG.xcodeproj -sdk iphoneos5.1 -configuration Release
+else
+if [ -n "$iphoneos43" ]; then
+    xcodebuild -project framework/TouchVG.xcodeproj -sdk iphoneos4.3 -configuration Release
+fi
 fi
 fi
 
