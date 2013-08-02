47105c30a4658410fb25ca6607a0e5333f27c824
Zhang Yungui <rhcad@hotmail.com>
2013-08-02 11:32:09 +0800

    Add testMagnifier1.

--- android/demo/src/vgdemo/testview/view/TestMagnifier1.java
+++ android/demo/src/vgdemo/testview/view/TestMagnifier1.java
@@ -4,23 +4,39 @@
 
 package vgdemo.testview.view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import touchvg.view.GraphView;
 
 //! 测试放大镜的布局视图类
-public class TestMagnifier1 extends FrameLayout {
+public class TestMagnifier1 extends LinearLayout {
 
     public TestMagnifier1(Context context) {
         super(context);
-                
+        this.setOrientation(VERTICAL);
+        
+        int flags = ((Activity) context).getIntent().getExtras().getInt("flags");
+        
+        final LayoutParams param = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        param.weight = 1;
+        
         final GraphView1 view = new GraphView1(context);
-        addView(view, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        addView(view, param);
         
         final GraphView magView = new GraphView(context, view);
-        addView(magView, new LayoutParams(200, 200));
+        addView(magView, param);
         magView.setBackgroundColor(Color.GREEN);
+        
+        if ((flags & 16) != 0) {
+            final GraphView1 view2 = new GraphView1(context);
+            addView(view2, param);
+            
+            final GraphView magView2 = new GraphView(context, view2);
+            addView(magView2, param);
+            magView2.setBackgroundColor(Color.BLUE);
+        }
     }
 
 }
